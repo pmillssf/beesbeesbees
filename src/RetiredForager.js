56d7585565e7b2e17f92fa1dc39849a1cec8f096
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-13 12:01:21 -0800

    Pass 1st RetiredForagerBee test
    Define RetiredForagerBee class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,9 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
 };
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
 
