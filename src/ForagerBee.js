fb607f74e19d3d86aba290eaeef6ef337b5fa2db
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-13 11:58:39 -0800

    Pass 7th ForagerBee test

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -3,6 +3,7 @@ var ForagerBee = function() {
   this.age = 10;
   this.job = 'find pollen';
   this.canFly = true;
+  this.treasureChest = [];
 };
 
 ForagerBee.prototype = Object.create(Bee.prototype);
