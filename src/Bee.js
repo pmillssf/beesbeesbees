1d3c008e06328866202ec31686819d825cd2f11e
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-13 11:44:14 -0800

    Define Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  // Grub.call(this);
+  Grub.call(this);
 };
+// set Bee.prototype to Object.create(Grub.prototype);
+Bee.prototype = Object.create(Grub.prototype);
+
+// set Bee.prototype.constructor to Bee
+Bee.prototype.constructor = Bee;
+
 
