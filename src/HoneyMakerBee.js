926fabd51e8e61686adb3d397e67a79c39c7bdca
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-13 11:50:39 -0800

    Pass 1st HoneyMakerBee test
    Define HoneyMakerBee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,8 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
