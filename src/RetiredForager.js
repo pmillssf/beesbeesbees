eb3318a951b3d41102aa0bc2615576823e353344
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-13 12:03:26 -0800

    Pass 4th RetiredForagerBee test

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -3,6 +3,7 @@ var RetiredForagerBee = function() {
   this.age = 40;
   this.job = 'gamble';
   this.canFly = false;
+  this.color = 'grey';
 };
 
 RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
